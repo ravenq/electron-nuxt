220ef851806c2a0d7908a560f6bd9a5c731f8e75
flw_dream@126.com <ravenq>
2018-04-08 23:49:17 +0800

    logo

--- components/AppLogo.vue
+++ components/AppLogo.vue
@@ -1,13 +1,31 @@
 <template>
-  <div class="VueToNuxtLogo">
-    <div class="Triangle Triangle--two"/>
-    <div class="Triangle Triangle--one"/>
-    <div class="Triangle Triangle--three"/>
-    <div class="Triangle Triangle--four"/>
+  <div class="logo">
+    <div class="VueToNuxtLogo">
+      <div class="Triangle Triangle--two"/>
+      <div class="Triangle Triangle--one"/>
+      <div class="Triangle Triangle--three"/>
+      <div class="Triangle Triangle--four"/>
+    </div>
+    <svg class="electron" viewBox="340 25 200 250" xmlns="http://www.w3.org/2000/svg">
+      <g fill="none" fill-rule="evenodd">
+        <g class="hero-icons">
+          <path class="hero-icon hero-icon-1  loop-3"  d="M441.1132 69.724c7.681 0 13.9075-6.207 13.9075-13.8636 0-7.6565-6.2266-13.8634-13.9075-13.8634-7.681 0-13.9076 6.207-13.9076 13.8634 0 7.6566 6.2266 13.8635 13.9076 13.8635zm0-5.7932c-4.4713 0-8.096-3.6132-8.096-8.0704 0-4.457 3.6247-8.0703 8.096-8.0703 4.4712 0 8.096 3.6133 8.096 8.0704 0 4.4572-3.6248 8.0704-8.096 8.0704z"/>
+          <path class="hero-icon hero-icon-2  loop-2"  d="M354.8995 220.2693c7.681 0 13.9075-6.207 13.9075-13.8635s-6.2266-13.8634-13.9075-13.8634c-7.681 0-13.9075 6.207-13.9075 13.8634 0 7.6566 6.2266 13.8635 13.9075 13.8635zm0-5.793c-4.4713 0-8.096-3.6133-8.096-8.0705 0-4.457 3.6247-8.0703 8.096-8.0703s8.096 3.6132 8.096 8.0703c0 4.4572-3.6247 8.0704-8.096 8.0704z"/>
+          <path class="hero-icon hero-icon-3  loop-1"  d="M541.0343 206.4058c0-7.6565-6.2266-13.8634-13.9075-13.8634-7.681 0-13.9075 6.207-13.9075 13.8634 0 7.6566 6.2266 13.8635 13.9075 13.8635 7.681 0 13.9075-6.207 13.9075-13.8635zm-5.8115 0c0 4.4572-3.6247 8.0704-8.096 8.0704s-8.096-3.6132-8.096-8.0704c0-4.457 3.6247-8.0703 8.096-8.0703s8.096 3.6132 8.096 8.0703z"/>
+          <path class="hero-icon hero-icon-4  short-3" d="M397.6943 214.5258c9.7012 27.0033 25.5723 43.629 43.419 43.629 13.0157 0 25.0578-8.8443 34.4482-24.4154.827-1.371.3822-3.1507-.9932-3.975-1.3755-.824-3.1607-.3808-3.9876.9902-8.439 13.9938-18.8052 21.6072-29.4675 21.6072-14.8247 0-28.9803-14.8288-37.9476-39.7892-.541-1.506-2.2044-2.2897-3.7153-1.7504-1.511.5394-2.297 2.1975-1.756 3.7036z"/>
+          <path class="hero-icon hero-icon-5  short-2" d="M514.124 163.4733c18.5545-21.85 25.033-43.826 16.122-59.2117-6.557-11.321-20.419-17.2982-38.841-17.537-1.6047-.021-2.9225 1.259-2.9434 2.8586-.0208 1.5996 1.263 2.9132 2.8678 2.934 16.5683.2148 28.5106 5.3642 33.8836 14.641 7.4018 12.7797 1.6243 32.3774-15.5247 52.5722-1.037 1.221-.8844 3.0487.3405 4.0822 1.2248 1.0336 3.0584.8817 4.0952-.3393z"/>
+          <path class="hero-icon hero-icon-6  short-1" d="M411.5672 88.457c-28.3373-5.1448-50.7424.24-59.672 15.6575-6.6635 11.505-4.7588 26.7585 4.6193 43.0637.7982 1.3878 2.574 1.8678 3.966 1.072 1.3923-.7956 1.874-2.5656 1.0756-3.9534-8.4477-14.688-10.0915-27.8524-4.628-37.2857 7.418-12.8074 27.403-17.6105 53.5978-12.8546 1.579.2866 3.092-.7568 3.3794-2.3307.2876-1.5738-.7592-3.082-2.338-3.3687z"/>
+          <path class="hero-icon hero-icon-7  long-3"  d="M486.3075 209.2436c5.022-15.998 7.7194-34.453 7.7194-53.6842 0-47.9875-16.849-89.3545-40.8478-99.977-1.4667-.649-3.1837.0098-3.835 1.472-.6512 1.462.01 3.1735 1.4766 3.8227 21.404 9.474 37.3945 48.7337 37.3945 94.6824 0 18.6574-2.612 36.5297-7.454 51.954-.4794 1.5268.3736 3.1518 1.9052 3.6295s3.1617-.3727 3.641-1.8994z"/>
+          <path class="hero-icon hero-icon-8  long-2"  d="M466.439 89.4215c-16.7763 3.583-34.6332 10.5886-51.7827 20.4585-42.434 24.4216-70.1147 60.4323-66.2703 86.5432.233 1.5828 1.709 2.6776 3.297 2.4453 1.5877-.2323 2.686-1.7037 2.453-3.2865-3.4135-23.1838 22.825-57.3183 63.426-80.685 16.6365-9.5746 33.9267-16.3578 50.0946-19.811 1.5692-.335 2.5687-1.8748 2.2325-3.439-.336-1.5642-1.8807-2.5606-3.45-2.2255z"/>
+          <path class="hero-icon hero-icon-9  long-1"  d="M371.2508 166.997c11.458 12.5516 26.3438 24.3243 43.3203 34.0947 41.106 23.6572 84.866 29.9805 106.4328 15.3217 1.326-.9013 1.668-2.7033.7638-4.025-.904-1.3217-2.712-1.6626-4.0378-.7614-19.302 13.1195-60.871 7.1128-100.253-15.5523-16.469-9.4783-30.8834-20.8782-41.9277-32.9767-1.08-1.1832-2.9178-1.2695-4.1048-.1928-1.187 1.0766-1.2735 2.9086-.1934 4.0918z"/>
+          <path class="hero-icon hero-icon-10 dot"     d="M443.2374 165.3634c-5.432 1.17-10.7838-2.2712-11.9598-7.686-1.1714-5.415 2.2785-10.7498 7.7106-11.922 5.432-1.17 10.7838 2.2712 11.9598 7.686 1.1737 5.415-2.2785 10.7498-7.7106 11.922z"/>
+        </g>
+      </g>
+    </svg>
   </div>
 </template>
 
-<style>
+<style lang="scss">
 .VueToNuxtLogo {
   display: inline-block;
   animation: turn 2s linear forwards 1s;
@@ -29,7 +47,7 @@
 .Triangle--one {
   border-left: 105px solid transparent;
   border-right: 105px solid transparent;
-  border-bottom: 180px solid #41B883;
+  border-bottom: 180px solid #41b883;
 }
 
 .Triangle--two {
@@ -38,7 +56,7 @@
   animation: goright 0.5s linear forwards 3.5s;
   border-left: 87.5px solid transparent;
   border-right: 87.5px solid transparent;
-  border-bottom: 150px solid #3B8070;
+  border-bottom: 150px solid #3b8070;
 }
 
 .Triangle--three {
@@ -47,7 +65,7 @@
   animation: goright 0.5s linear forwards 3.5s;
   border-left: 70px solid transparent;
   border-right: 70px solid transparent;
-  border-bottom: 120px solid #35495E;
+  border-bottom: 120px solid #35495e;
 }
 
 .Triangle--four {
@@ -75,5 +93,129 @@
   100% {
     left: 70px;
   }
+}
+
+// electron
+.electron {
+  width: 200px;
+  height: 200px;
+}
+.hero-icons {
+  transform-origin: 50% 50%;
+}
+
+.hero-icon.loop-3 {
+  transform: translate(79px, 21px);
+  opacity: 1;
+}
+
+.hero-icon {
+  fill: #9feaf9;
+  opacity: 1;
+  transform-origin: 50% 50%;
+}
+
+$grid-breakpoints: (
+  // Extra small screen / phone
+  xs: 0,
+  // Small screen / phone
+  sm: 544px,
+  // Medium screen / tablet
+  md: 768px,
+  // Large screen / desktop
+  lg: 992px,
+  // Extra large screen / wide desktop
+  xl: 1200px
+) !default;
+
+// HACK! $grid-breakpoints and $breakpoints are dupes. See https://git.io/vNPI4
+$breakpoints: (
+  // Extra small screen / phone
+  xs: 0,
+  // Small screen / phone
+  sm: 544px,
+  // Medium screen / tablet
+  md: 768px,
+  // Large screen / desktop
+  lg: 992px,
+  // Extra large screen / wide desktop
+  xl: 1200px
+) !default;
+
+$grid-breakpoints: map-merge($grid-breakpoints, (xl: 1300px));
+
+@function breakpoint-min($name, $breakpoints: $grid-breakpoints) {
+  $min: map-get($breakpoints, $name);
+  @return if($min != 0, $min, null);
+}
+
+// Media of at least the minimum breakpoint width. No query for the smallest breakpoint.
+// Makes the @content apply to the given breakpoint and wider.
+@mixin breakpoint($name, $breakpoints: $grid-breakpoints) {
+  $min: breakpoint-min($name, $breakpoints);
+  @if $min {
+    @media (min-width: $min) {
+      @content;
+    }
+  } @else {
+    @content;
+  }
+}
+
+.hero {
+  margin: 0 auto;
+  backface-visibility: hidden;
+  max-width: 900px;
+  @include breakpoint(xl) { max-width: 100%; }
+  animation: hero-animation 1s forwards;
+}
+
+@keyframes hero-animation {
+    0% { opacity: 0; transform: scale(.96); }
+  100% { opacity: 1; transform: scale(1); }
+}
+
+// Hero icons
+.hero-icons {
+  transform-origin: 50% 50%;
+  animation: hero-icons-animation 2s 2.5s cubic-bezier(.50, .10, .50, .90) both;
+}
+@keyframes hero-icons-animation {
+  50% { transform: scale(1.1); }
+}
+
+// Electron Icon
+
+$hero-icon-delay: 1s;
+$hero-icon-delay-diff: .2s;
+$hero-icon-duration: 1.2s;
+$hero-icon-all-delay: 15 * $hero-icon-delay-diff + $hero-icon-delay;
+
+// Initial position
+.hero-icon.dot     { transform: translate(35px, -37px); opacity: 1; }
+.hero-icon.short-1 { transform: translate(119px, 37px) rotate(-134deg); }
+.hero-icon.short-2 { transform: translate(-20px, 61px) rotate(106deg); }
+.hero-icon.short-3 { transform: translate(65px, -18px) rotate(-17deg); }
+.hero-icon.long-1  { transform: translate(-101px, -45px) rotate(-107deg); }
+.hero-icon.long-2  { transform: translate(-35px, 8px) rotate(133deg); opacity: 1; }
+.hero-icon.long-3  { transform: translate(-64px, 20px) rotate(13deg); }
+.hero-icon.loop-1  { transform: translate(-51px, -128px); }
+.hero-icon.loop-2  { transform: translate(166px, -88px); }
+.hero-icon.loop-3  { transform: translate(79px, 21px); opacity: 1; }
+
+@for $i from 1 through 10 {
+  .hero-icon-#{$i} {
+    $_duration: $hero-icon-duration;
+    $_delay:   $hero-icon-delay + ($hero-icon-delay-diff * $i);
+    $_delay-o: $hero-icon-all-delay - ($hero-icon-delay-diff / $i);
+    animation: hero-icon-animation-t $_duration $_delay cubic-bezier(.50, 0, .20, 1) both,
+               hero-icon-animation-o .4s $_delay-o both;
+  }
+}
+@keyframes hero-icon-animation-t {
+  100% { transform: translate(0, 0) rotate(0deg); }
+}
+@keyframes hero-icon-animation-o {
+  100% { opacity: 1; }
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -6,15 +6,15 @@
         electron-nuxt
       </h1>
       <h2 class="subtitle">
-        Nuxt.js project
+        electron-nuxt project
       </h2>
       <div class="links">
         <a
-          href="https://nuxtjs.org/"
+          href="https://ravenq.github.io/"
           target="_blank"
           class="button--green">Documentation</a>
         <a
-          href="https://github.com/nuxt/nuxt.js"
+          href="https://github.com/ravenq/electron-nuxt"
           target="_blank"
           class="button--grey">GitHub</a>
       </div>
@@ -23,13 +23,13 @@
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
+import AppLogo from "~/components/AppLogo.vue";
 
 export default {
   components: {
     AppLogo
   }
-}
+};
 </script>
 
 <style>
@@ -42,7 +42,8 @@ export default {
 }
 
 .title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
   display: block;
   font-weight: 300;
   font-size: 100px;
